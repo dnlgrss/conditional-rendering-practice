89a110bd997de062c091c5e3db31c0eb70089579
dnlgrs <64085659+dnlgrss@users.noreply.github.com>
2021-12-17 10:26:51 +0100

    Comment out

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Form from "./Form";
 
 var userIsRegistered = false;
 
+//Passing usierIsRegistered value as props so that can be read in form fisrt then button component
 function App() {
   return (
     <div className="container">
